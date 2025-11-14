73667bfac63ff4c0c2e7b7c0f5cf681e4e3d3e9d
Nathan Erickson <nwericks@gmail.com>
2025-11-14 14:26:39 -0500

    added server client disconnect message

--- types/index.d.ts
+++ types/index.d.ts
@@ -7,6 +7,7 @@ declare module 'ssh2' {
 
     interface Connection {
         openssh_authAgent(callback: ServerCallback): void;
+        end(reason?: number | null, message?: string, lang?: string): this;
     }
 }
 
