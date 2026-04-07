d843529d38a75fdfd83643750ed7811837c0bdb8
Nathan Erickson <nwericks@gmail.com>
2026-04-07 14:35:54 -0400

    fix: restore Client.end() disconnect reason/message support lost in rebase

--- types/index.d.ts
+++ types/index.d.ts
@@ -6,6 +6,7 @@ declare module 'ssh2' {
     }
 
     interface Connection {
+        end(reason?: number | null, message?: string, lang?: string): this;
         openssh_authAgent(callback: ServerCallback): void;
     }
 }
